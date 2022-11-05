775bbace863c95a32ce67fd807ece6f11ded3130
Faizul Osman <faizulosmanrabby@gmail.com>
2022-11-05 18:37:18 +0600

    update with pagination

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,31 +1,41 @@
-import React from 'react';
-import './Cart.css';
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "./Cart.css";
 
-const Cart = (props) => {
-    const { cart, clearCart, children } = props;
-    
-    let total = 0;
-    let shipping = 0;
-    let quantity = 0;
-    for(const product of cart){
-        quantity = quantity + product.quantity;
-        total = total + product.price * product.quantity;
-        shipping = shipping + product.shipping;
-    }
-    const tax = parseFloat((total * 0.1).toFixed(2));
-    const grandTotal = total + shipping + tax;
-    return (
-        <div className='cart'>
-            <h4>Order Summary</h4>
-            <p>Selected Items: {quantity}</p>
-            <p>Total price: ${total}</p>
-            <p>Total Shipping: ${shipping}</p>
-            <p>Tax: {tax}</p>
-            <h5>Grand Total: {grandTotal.toFixed(2)}</h5>
-            <button onClick={clearCart}>Clear Cart</button>
-            {children}
-        </div>
-    );
+const Cart = ({ cart, clearCart, children }) => {
+  let quantity = 0;
+  let total = 0;
+  let shipping = 0;
+
+  for (const product of cart) {
+    quantity = quantity + product.quantity;
+    total = total + product.price * product.quantity;
+    shipping = shipping + product.shipping * product.quantity;
+  }
+
+  const tax = total / 10;
+  const grandTotal = total + shipping + tax;
+
+  return (
+    <div className="cart">
+      <h3 className="heading">Order Summary</h3>
+      <div className="cart-details">
+        <p>Selected Items: {quantity}</p>
+        <p>Total Price: $ {total}</p>
+        <p>Total Shipping Charge: ${shipping}</p>
+        <p>Tax: ${tax.toFixed(2)}</p>
+        <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
+      </div>
+      <div className="buttons">
+        <button onClick={clearCart} className="clear-cart-btn">
+          <span>Clear Cart</span>
+          <FontAwesomeIcon icon={faTrash}></FontAwesomeIcon>
+        </button>
+      </div>
+      {children}
+    </div>
+  );
 };
 
 export default Cart;
